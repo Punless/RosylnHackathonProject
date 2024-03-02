63d18c28376e0b41235fd4354ea014848c9ec3e5
perfectlava <gologolomaps@gamil.com>
2024-03-02 13:21:42 -0500

    firebase setup

--- app/src/main/java/com/example/rosylnhackathonproject/CreateAccount.java
+++ app/src/main/java/com/example/rosylnhackathonproject/CreateAccount.java
@@ -5,18 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateAccount extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
-
+        mAuth = FirebaseAuth.getInstance();
         EditText email = findViewById(R.id.editTextTextEmailAddress3);
         EditText username = findViewById(R.id.editTextUsername);
         EditText editText = findViewById(R.id.editTextTextPassword);
         Button button = findViewById(R.id.button);
 
     }
+
 }
